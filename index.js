3f862def6eac8b2feec7e37eaa9fb5eb8b4b92dd
aliggins0303 <alex.l@southbendcodeschool.com>
2018-09-13 11:17:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
   $(document).ready(init)
 
-  // Initialize Firebase
- var config = {
-   apiKey: "AIzaSyCWhIhyFcWw60fmrOJWyy89A_s4YeGeU_c",
-   authDomain: "instructor-portal.firebaseapp.com",
-   databaseURL: "https://instructor-portal.firebaseio.com",
-   projectId: "instructor-portal",
-   storageBucket: "instructor-portal.appspot.com",
-   messagingSenderId: "337391615201"
- };
+//   // Initialize Firebase
+//  var config = {
+//    apiKey: "AIzaSyCWhIhyFcWw60fmrOJWyy89A_s4YeGeU_c",
+//    authDomain: "instructor-portal.firebaseapp.com",
+//    databaseURL: "https://instructor-portal.firebaseio.com",
+//    projectId: "instructor-portal",
+//    storageBucket: "instructor-portal.appspot.com",
+//    messagingSenderId: "337391615201"
+//  };
 
 
   function init() {
